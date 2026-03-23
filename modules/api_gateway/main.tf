010e6faba294bc77618a0bfd62f6c998f848a6b7
Yuliia Rybchak <rybchakyulya@gmail.com>
2026-03-23 22:51:59 +0200

    add terraform modules and configuration)

---
+++ modules/api_gateway/main.tf
@@ -0,0 +1,48 @@
+variable "api_name" {
+  type = string
+}
+
+variable "lambda_invoke_arn" {
+  type = string
+}
+
+variable "lambda_function_name" {
+  type = string
+}
+
+resource "aws_apigatewayv2_api" "http_api" {
+  name          = var.api_name
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.http_api.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id                 = aws_apigatewayv2_api.http_api.id
+  integration_type       = "AWS_PROXY"
+  integration_method     = "POST"
+  integration_uri        = var.lambda_invoke_arn
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "put_status" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "PUT /orders/{id}/status"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+resource "aws_lambda_permission" "api_gw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = var.lambda_function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
+}
+
+output "api_endpoint" {
+  value = aws_apigatewayv2_api.http_api.api_endpoint
+}
---
+++ modules/lambda/main.tf
@@ -0,0 +1,112 @@
+variable "function_name" {
+  type = string
+}
+
+variable "source_file" {
+  type = string
+}
+
+variable "dynamodb_table_arn" {
+  type = string
+}
+
+variable "dynamodb_table_name" {
+  type = string
+}
+
+variable "sns_topic_arn" {
+  type = string
+}
+
+variable "log_bucket_name" {
+  type = string
+}
+
+variable "log_bucket_arn" {
+  type = string
+}
+
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = var.source_file
+  output_path = "${path.module}/app.zip"
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "${var.function_name}-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy" "custom_access" {
+  name = "${var.function_name}-custom-policy"
+  role = aws_iam_role.lambda_exec.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:UpdateItem",
+          "dynamodb:GetItem",
+          "dynamodb:PutItem"
+        ]
+        Resource = var.dynamodb_table_arn
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "sns:Publish"
+        ]
+        Resource = var.sns_topic_arn
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject"
+        ]
+        Resource = "${var.log_bucket_arn}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_lambda_function" "api_handler" {
+  filename         = data.archive_file.lambda_zip.output_path
+  function_name    = var.function_name
+  role             = aws_iam_role.lambda_exec.arn
+  handler          = "app.handler"
+  runtime          = "python3.12"
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+
+  environment {
+    variables = {
+      TABLE_NAME = var.dynamodb_table_name
+      TOPIC_ARN  = var.sns_topic_arn
+      LOG_BUCKET = var.log_bucket_name
+    }
+  }
+}
+
+output "invoke_arn" {
+  value = aws_lambda_function.api_handler.invoke_arn
+}
+
+output "function_name" {
+  value = aws_lambda_function.api_handler.function_name
+}
